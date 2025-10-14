f7c005579933ff15e462ea5c497efa6efe4f1c94
egigj-dev <egigj.dev@gmail.com>
2025-10-14 17:51:38 +0200

    Added documentation

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
+"""Script that calculates the shape of a matrix"""
+
+
 def matrix_shape(matrix):
-    """
-    Module that defines a function to compute the shape of a matrix.
-
-    Functions:
-        matrix_shape(matrix): Returns the dimensions of a matrix as a list.
-
-    Parameters: matrix that needs shape defined
-    """
+    """Script that calculates the shape of a matrix"""
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
