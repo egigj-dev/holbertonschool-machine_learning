6139dcff616b322bdaad316d1d9e668ec326fb8b
egigj-dev <egigj.dev@gmail.com>
2025-10-14 18:11:38 +0200

    fix: pycodestyle req

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 """Script that returns the transpose of a 2D matrix"""
 
+
 def matrix_transpose(matrix):
-    """
-    matrix = [[1, 2, 3]
-              [4, 5, 6]]
-    
-    transposed_matrix = [[1, 4]
-                         [2, 5]
-                         [3, 6]]
-    """
+    """Script that returns the transpose of a 2D matrix"""
     transposed_matrix = []
     for i in range(len(matrix[0])):
         new_row = []
@@ -18,4 +12,3 @@ def matrix_transpose(matrix):
         transposed_matrix.append(new_row)
     
     return transposed_matrix
-    #return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
