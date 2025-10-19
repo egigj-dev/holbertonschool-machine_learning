37ce8c0783e2e709e0d591b1455782bf80a429b4
egigj-dev <egigj.dev@gmail.com>
2025-10-19 20:40:19 +0200

    add exc.9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Summation of the first n integers."""
+
+def summation_i_squared(n):
+    """Summation of the first n integers."""
+    if isinstance(n, int) and n > 0:
+        squares = [i**2 for i in range(1, n + 1)]
+        return sum(squares)
+    else:
+        return None
