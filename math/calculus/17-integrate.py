591e0189b7898c0d15af237fd8cbda5159fb8238
egigj-dev <egigj.dev@gmail.com>
2025-10-21 01:12:00 +0200

    add: exc.15-17

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,9 @@
+#!usr/bin/env python3
+"""Integrate a polynomial function."""
+
+
+def poly_integral(poly, C=0):
+    """Integrate a polynomial function."""
+    if not isinstance(poly, list) or len(poly) < 1:
+        return None
+    return [C] + [poly[i] / (i + 1) for i in range(len(poly))]
