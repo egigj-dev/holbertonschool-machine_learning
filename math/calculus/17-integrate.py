0c040d83e1364d2e6794ae614086905f5b648278
egigj-dev <egigj.dev@gmail.com>
2025-10-21 01:21:18 +0200

    fix: exc.17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,6 +4,14 @@
 
 def poly_integral(poly, C=0):
     """Integrate a polynomial function."""
-    if not isinstance(poly, list) or len(poly) < 1:
+    if (not isinstance(poly, list) or
+        not all(isinstance(c, (int, float)) for c in poly) or
+        not isinstance(C, (int, float))):
         return None
-    return [C] + [poly[i] / (i + 1) for i in range(len(poly))]
+    result = [C] + [
+        int(c / (i + 1)) if (c / (i + 1)).is_integer() else c / (i + 1)
+        for i, c in enumerate(poly)
+    ]
+    while len(result) > 1 and result[-1] == 0:
+        result.pop()
+    return result
