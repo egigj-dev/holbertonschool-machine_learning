5f2ce00f90171a401e35d0646783ae3520b325c3
egigj-dev <egigj.dev@gmail.com>
2025-11-02 02:58:53 +0100

    Add: exc. 1-5

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Script that manipulates a dataframe column"""
 import pandas as pd
-
+from_file = __import__('2-from_file').from_file
 
 def rename(df):
     """Renames a dataframe column"""
-    df = df.rename(columns={'Timestamp ': 'Datetime'})
+    df = df.rename(columns={'Timestamp': 'Datetime'})
     df['Datetime'] = pd.to_datetime(df['Datetime'])
     return df[['Datetime', 'Close']]
