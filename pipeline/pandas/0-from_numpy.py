3e9bea6293aca6c25cf94128fdb0aca35176e105
egigj-dev <egigj.dev@gmail.com>
2025-11-02 02:22:10 +0100

    Add: exc. 1-5

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
 """ Script that creates a Pandas Dataframe from a Numpy array """
 import pandas as pd
-import numpy as np
-
 
 def from_numpy(array):
     """Creates a pandas DataFrame from a NumPy ndarray."""
-    if not isinstance(array, np.ndarray):
-        raise TypeError("Input must be a NumPy ndarray.")
     num_cols = array.shape[1] if array.ndim > 1 else 1
     columns = [chr(65 + i) for i in range(num_cols)]
     df = pd.DataFrame(array, columns=columns)
