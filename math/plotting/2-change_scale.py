2ae0cb9d338bba5f6ef5a9231fdc274c2113fcee
egigj-dev <egigj.dev@gmail.com>
2025-11-04 19:10:06 +0100

    Add exc. 3

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Script that plots two line graphs"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    """Method that plots two line graphs"""
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(x, y)
+    plt.yscale('log')
+    plt.xlim(x[0], x[-1])
+    plt.title("Exponential Decay of C-14")
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.show()
