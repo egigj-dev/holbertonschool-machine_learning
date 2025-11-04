32021a93a2211c5d9517f3da03dae404a85b4399
egigj-dev <egigj.dev@gmail.com>
2025-11-04 19:14:51 +0100

    Add exc. 5

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Script that plots a histogram graph"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Function that plots a histogram graph"""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.hist(student_grades, bins=np.arange(0, 101, 10), edgecolor='k')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.title("Project A")
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.xticks(np.arange(0, 101, step=10))
+    plt.show()
