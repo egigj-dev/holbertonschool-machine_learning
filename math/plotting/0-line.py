b4340a6607dc72ba058d3b9f9a3aad2491711acb
egigj-dev <egigj.dev@gmail.com>
2025-11-04 19:05:13 +0100

    Add exc. 1

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Script that plots a line graph"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """Script that plots a line graph"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(y, color='r')
