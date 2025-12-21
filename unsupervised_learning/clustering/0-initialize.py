be75cc0db7caff075c2b7b3e77f481339da0445f
egigj-dev <egigj.dev@gmail.com>
2025-12-21 18:33:55 +0100

    Add clustering tasks

---
+++ unsupervised_learning/clustering/0-initialize.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""K-means clustering initialization module"""
+import numpy as np
+
+
+def initialize(X, k):
+    """Initialize cluster centroids for K-means"""
+
+    # # Input validation
+    n, d = X.shape
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if isinstance(k, int) is False or k <= 0:
+        return None
+    
+    # Check if k is reasonable (not more data points than we have)
+    if k > n:
+        return None
+    
+    # Find minimum and maximum values along each dimension
+    low = X.min(axis=0)
+    high = X.max(axis=0)
+
+    # Randomly generate centroids
+    return np.random.uniform(low, high, size=(k, d))
+    return np.random.uniform(X.min(axis=0), X.max(axis=0), (k, X.shape[1]))
