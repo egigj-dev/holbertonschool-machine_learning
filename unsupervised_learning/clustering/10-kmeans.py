bb26b7e6e9475a8c73e775569c6820ddf1eafd00
egigj-dev <egigj.dev@gmail.com>
2025-12-26 16:44:52 +0100

    Add: exc. 3-11

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -5,13 +5,14 @@ import sklearn.cluster
 
 def kmeans(X, k):
     """Performs K-means on a dataset."""
-     
     n, d = X.shape
     if not isinstance(k, int) or k <= 0 or k > n:
         return None, None
+
     # Create and fit K-means model
     kmeans_model = sklearn.cluster.KMeans(n_clusters=k)
     kmeans_model.fit(X)
+
     # Extract results
     C = kmeans_model.cluster_centers_
     clss = kmeans_model.labels_
