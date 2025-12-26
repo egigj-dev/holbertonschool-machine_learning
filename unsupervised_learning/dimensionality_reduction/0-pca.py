805a8bfb6307af10108701655eda64d5e73c80fa
egigj-dev <egigj.dev@gmail.com>
2025-12-26 17:19:43 +0100

    Fix: exc. 0

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,35 +1,21 @@
 #!/usr/bin/env python3
-"""Principal Component Analysis (PCA) module"""
+"""PCA Module"""
 import numpy as np
-
 
 def pca(X, var=0.95):
     """
-    Performs PCA on a dataset.
+    Performs Principal Component Analysis (PCA) on a dataset.
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None
-    if not isinstance(var, (int, float)) or var <= 0 or var > 1:
-        return None
+    # Compute SVD of X
+    U, S, Vh = np.linalg.svd(X)
+    V = Vh.T  # Columns are principal components
 
-    # Compute SVD
-    U, S, Vt = np.linalg.svd(X, full_matrices=False)
+    # Compute cumulative variance fraction
+    cum_var = np.cumsum(S) / np.sum(S)
 
-    # Eigenvalues of covariance matrix
-    eigenvalues = (S ** 2) / X.shape[0]
+    # Determine number of components to keep
+    num_components = np.where(cum_var >= var)[0][0] + 1
 
-    # Principal components
-    W = Vt.T
-
-    # Determine how many components to keep
-    cum_var = np.cumsum(eigenvalues) / np.sum(eigenvalues)
-    nd = np.searchsorted(cum_var, var) + 1
-    W = W[:, :nd]
-
-    # Optional: enforce sign convention
-    for i in range(W.shape[1]):
-        max_idx = np.abs(W[:, i]).argmax()
-        if W[max_idx, i] < 0:
-            W[:, i] *= -1
-
+    # Return the first 'num_components' principal components
+    W = V[:, :num_components]
     return W
