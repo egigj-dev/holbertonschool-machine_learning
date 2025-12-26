7dd2d791f8bb6a02042ec1133f5a4ca76852fcca
egigj-dev <egigj.dev@gmail.com>
2025-12-26 17:24:28 +0100

    Fix: exc. 1

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python3
-"""PCA dimensionality reduction to a fixed number of components"""
+"""PCA 1 Module"""
 import numpy as np
 
 
 def pca(X, ndim):
     """
-    Performs PCA on a dataset and reduces it to 'ndim' dimensions.
+    Performs PCA on a dataset
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None
-    n, d = X.shape
-    if not isinstance(ndim, int) or ndim <= 0 or ndim > d:
-        return None
+    X_m = X - np.mean(X, axis=0)
 
-    # Center the data (zero mean)
-    X_centered = X - np.mean(X, axis=0)
+    U, S, Vh = np.linalg.svd(X_m)
 
-    # Compute SVD
-    U, S, Vh = np.linalg.svd(X_centered, full_matrices=False)
-
-    # Principal components (take the first 'ndim' components)
     W = Vh.T[:, :ndim]
 
-    # Project the data onto the principal components
-    T = X_centered @ W
-
-    return T
+    return X_m @ W
