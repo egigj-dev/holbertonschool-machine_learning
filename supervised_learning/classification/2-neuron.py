066ffd992cd9f45d5628872be27f99ec6dc35908
egigj-dev <egigj.dev@gmail.com>
2026-01-31 21:33:39 +0100

    Builing a perceptron

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""A script that defines a single neuron."""
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron."""
+
+    def __init__(self, nx):
+        """Initialize the neuron with private attributes."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1/ (1 + np.exp(-Z))
+
+        return self.__A
