e91534742f54538605922c0ffc9b25edfa1cacd2
egigj-dev <egigj.dev@gmail.com>
2026-01-31 21:46:40 +0100

    Builing a perceptron

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -39,7 +39,7 @@ class Neuron:
         Z = np.matmul(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-Z))
         return self.__A
-    
+
     def cost(self, Y, A):
         """
         Calculates the cost of loss function.
