5a9c076c0a8636dd99b15c65ed7648c84770273a
egigj-dev <egigj.dev@gmail.com>
2026-02-03 17:46:59 +0100

    add exc. 14

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+
+def create_batch_norm_layer(prev, n, activation):
+    """
+    Creates a batch normalization layer for a neural network.
+
+    Args:
+        prev: activated output of the previous layer
+        n: number of nodes in the layer to be created
+        activation: activation function to apply
+
+    Returns:
+        Tensor of the activated output for the layer
+    """
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+
+    # Dense layer without activation
+    dense = tf.keras.layers.Dense(
+        n,
+        kernel_initializer=initializer,
+        use_bias=False
+    )(prev)
+
+    # Trainable batch norm parameters
+    gamma = tf.Variable(tf.ones([n]), trainable=True)
+    beta = tf.Variable(tf.zeros([n]), trainable=True)
+
+    # Batch statistics
+    mean, variance = tf.nn.moments(dense, axes=[0])
+
+    # Batch normalization
+    batch_norm = tf.nn.batch_normalization(
+        dense,
+        mean,
+        variance,
+        beta,
+        gamma,
+        variance_epsilon=1e-7
+    )
+
+    # Activation
+    return activation(batch_norm)
