e008d0d6247099439fa8a03587918f3b63b9296d
egigj-dev <egigj.dev@gmail.com>
2026-02-14 23:42:51 +0100

    fix task 2

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -5,11 +5,11 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
-    Updates weights and biases of a neural network using gradient descent
-    with L2 regularization.
+    Updates the weights and biases of a neural network using
+    gradient descent with L2 regularization.
 
-    Parameters:
-    -----------
+    Parameters
+    ----------
     Y : numpy.ndarray of shape (classes, m)
         One-hot correct labels
     weights : dict
@@ -24,24 +24,22 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         Number of layers in the network
     """
     m = Y.shape[1]
-    
-    # Start backpropagation from output layer
-    # Gradient of softmax + cross-entropy loss
+
+    # Start from the output layer (softmax + cross-entropy)
     dZ = cache['A' + str(L)] - Y
-    
-    # Backpropagate through all layers
+
     for layer in range(L, 0, -1):
         A_prev = cache['A' + str(layer - 1)]
-        
-        # Compute gradients with L2 regularization term
+
+        # Gradient of weights with L2 regularization
         dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * weights['W' + str(layer)]
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-        
-        # Update weights and biases in place
+
+        # Update weights and biases
         weights['W' + str(layer)] -= alpha * dW
         weights['b' + str(layer)] -= alpha * db
-        
-        # Compute dZ for previous layer (don't compute for layer 0)
+
+        # Backpropagate to previous layer
         if layer > 1:
-            # Derivative of tanh: 1 - tanh(x)^2
+            # Derivative of tanh: 1 - tanh^2
             dZ = np.matmul(weights['W' + str(layer)].T, dZ) * (1 - np.power(cache['A' + str(layer - 1)], 2))
