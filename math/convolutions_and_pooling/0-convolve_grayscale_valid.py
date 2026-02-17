acdef89c17118d9db6c5d2c3eb63bcd38e70be09
egigj-dev <egigj.dev@gmail.com>
2026-02-17 17:23:32 +0100

    task 0: pycodestyle

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -21,7 +21,7 @@ def convolve_grayscale_valid(images, kernel):
         for j in range(ow):
             # Extract the region from all images at once using slicing
             region = images[:, i:i+kh, j:j+kw]  # shape: (m, kh, kw)
- 
+
             # Element-wise multiply with kernel and sum
             output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
 
