b97ab084623fb1fc842a0c80a504be66b470c1f8
egigj-dev <egigj.dev@gmail.com>
2026-03-03 01:56:14 +0100

    fix exc 3

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""Module for performing convolution on grayscale images"""
+"""Module for performing convolution on images with multiple kernels"""
 import numpy as np
 
 
-def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+def convolve(images, kernels, padding='same', stride=(1, 1)):
     """
-    Performs a convolution on grayscale images
+    Performs a convolution on images using multiple kernels
     """
-    m, h, w = images.shape
-    kh, kw = kernel.shape
+    m, h, w, c = images.shape
+    kh, kw, kc, nc = kernels.shape
     sh, sw = stride
 
     # Determine padding
@@ -23,30 +23,29 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     # Pad images
     padded = np.pad(
         images,
-        ((0, 0), (ph, ph), (pw, pw)),
+        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
         mode='constant'
     )
 
-    # Compute output dimensions
+    # Output dimensions
     out_h = (h + 2 * ph - kh) // sh + 1
     out_w = (w + 2 * pw - kw) // sw + 1
 
-    output = np.zeros((m, out_h, out_w))
+    output = np.zeros((m, out_h, out_w, nc))
 
-    # Convolution (only two loops)
-    for i in range(out_h):
-        for j in range(out_w):
+    # Convolution (three loops: over kernels, height, width)
+    for k in range(nc):
+        kernel = kernels[:, :, :, k]
+        for i in range(out_h):
             vert_start = i * sh
             vert_end = vert_start + kh
-            horiz_start = j * sw
-            horiz_end = horiz_start + kw
+            for j in range(out_w):
+                horiz_start = j * sw
+                horiz_end = horiz_start + kw
 
-            region = padded[:, vert_start:vert_end,
-                            horiz_start:horiz_end]
+                region = padded[:, vert_start:vert_end,
+                                horiz_start:horiz_end, :]
 
-            output[:, i, j] = np.sum(
-                region * kernel,
-                axis=(1, 2)
-            )
+                output[:, i, j, k] = np.sum(region * kernel, axis=(1, 2, 3))
 
     return output
