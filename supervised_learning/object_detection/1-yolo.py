e77922de2c68e833ffec75cf92c184e02609a1d1
egigj-dev <egigj.dev@gmail.com>
2026-03-22 23:54:58 +0100

    fix bounding box calculations

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -19,60 +19,54 @@ class Yolo:
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
-
+    
     def process_outputs(self, outputs, image_size):
         """
-        Process Darknet model outputs for a single image
-        Parameters:
-        - outputs: list of numpy.ndarrays, predictions from the Darknet model
-        - image_size: np.ndarray, original image size [height, width]
+            Process Darknet model outputs for a single image
+            Parameters:
+            - outputs: list of numpy.ndarrays, predictions from the Darknet model
+            - image_size: np.ndarray, original image size [height, width]
 
-        Returns:
-        - boxes: list of np.ndarrays of shape (grid_h, grid_w, anchor_boxes, 4) containing
-                 (x1, y1, x2, y2) relative to original image
-        - box_confidences: list of np.ndarrays of shape (grid_h, grid_w, anchor_boxes, 1)
-        - box_class_probs: list of np.ndarrays of shape (grid_h, grid_w, anchor_boxes, classes)
+            Returns:
+            - boxes: list of np.ndarrays of shape (grid_h, grid_w, anchor_boxes, 4) containing
+                    (x1, y1, x2, y2) relative to original image
+            - box_confidences: list of np.ndarrays of shape (grid_h, grid_w, anchor_boxes, 1)
+            - box_class_probs: list of np.ndarrays of shape (grid_h, grid_w, anchor_boxes, classes)
         """
         boxes = []
         box_confidences = []
         box_class_probs = []
-
         image_height, image_width = image_size
-        input_h = self.model.input.shape[1]
-        input_w = self.model.input.shape[2]
+        
+        input_w = self.model.input.shape[1]
+        input_h = self.model.input.shape[2]
 
         for i, output in enumerate(outputs):
             grid_h, grid_w, anchor_boxes, _ = output.shape
 
-            # Extract t_x, t_y, t_w, t_h, box_confidence, class_probs
             t_xy = output[..., 0:2]
             t_wh = output[..., 2:4]
             box_conf = output[..., 4:5]
             class_probs = output[..., 5:]
 
-            # Sigmoid for xy and box confidence
             sigmoid_xy = 1 / (1 + np.exp(-t_xy))
             box_confidence = 1 / (1 + np.exp(-box_conf))
             class_prob = 1 / (1 + np.exp(-class_probs))
 
-            # Create grid
             grid_x = np.arange(grid_w)
             grid_y = np.arange(grid_h)
             cx, cy = np.meshgrid(grid_x, grid_y)
             cx = np.expand_dims(cx, axis=-1)
             cy = np.expand_dims(cy, axis=-1)
 
-            # bx, by: center coordinates relative to image (0-1)
             bx = (sigmoid_xy[..., 0] + cx) / grid_w
             by = (sigmoid_xy[..., 1] + cy) / grid_h
 
-            # bw, bh: width and height relative to image
             anchor_w = self.anchors[i, :, 0]
             anchor_h = self.anchors[i, :, 1]
             bw = (anchor_w * np.exp(t_wh[..., 0])) / input_w
             bh = (anchor_h * np.exp(t_wh[..., 1])) / input_h
 
-            # Convert to corner coordinates in original image scale
             x1 = (bx - bw / 2) * image_width
             y1 = (by - bh / 2) * image_height
             x2 = (bx + bw / 2) * image_width
@@ -84,4 +78,3 @@ class Yolo:
             box_class_probs.append(class_prob)
 
         return boxes, box_confidences, box_class_probs
-
