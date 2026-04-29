923a102713897e684129a3a3df63b6a3154cde65
egigj-dev <egigj.dev@gmail.com>
2026-04-29 19:50:10 +0200

    fix pycodestyle issues

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -15,7 +15,7 @@ class RNNCell:
 
     def softmax(self, x):
         """Softmax activation function"""
-        
+  
         e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
         return e_x / np.sum(e_x, axis=1, keepdims=True)
 
