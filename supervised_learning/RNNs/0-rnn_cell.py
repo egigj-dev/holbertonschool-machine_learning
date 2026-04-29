044b18eca24ef7e5c9af5439cbf64d34d9fe18f2
egigj-dev <egigj.dev@gmail.com>
2026-04-29 19:49:18 +0200

    change activation to softmax

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -13,9 +13,19 @@ class RNNCell:
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
+    def softmax(self, x):
+        """Softmax activation function"""
+        
+        e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return e_x / np.sum(e_x, axis=1, keepdims=True)
+
     def forward(self, h_prev, x_t):
         """Forward propagation method"""
-        h_next = np.tanh(np.matmul(np.concatenate((h_prev, x_t), axis=1),
-                                   self.Wh) + self.bh)
-        y = np.matmul(h_next, self.Wy) + self.by
+
+        concat = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.matmul(concat, self.Wh) + self.bh)
+
+        y_linear = np.matmul(h_next, self.Wy) + self.by
+        y = self.softmax(y_linear)
+
         return h_next, y
